12675747910408468494f58d6f3bb001c21fa99a
JustAbhi77 <ojhaabhinav2016@gmail.com>
2023-11-17 23:18:45 +0530

    Turn based click,squashed screen resize bug.

--- Scripts/Screen_Res.gd
+++ Scripts/Screen_Res.gd
@@ -3,12 +3,22 @@ extends Node
 var cellSprite = preload("res://Scenes/Cell.tscn")
 var viewport: Viewport
 var pooledCells: Array = []
-var boardSize: float
-var cellSize: float
+var boardSize_min: float
+var boardSize_max: float
+var cellSize_min: float
+var cellSize_max: float
 var boardCenter: Vector2
 var cellsStartOffset: Vector2
+var cellsOffset: Vector2
 var BoardStart: Vector2
 var scale: Vector2
+var BlackColor: Color = Color(0, 0, 0)
+var isRedTurn: bool = true
+var RedTColor: Color = Color(1, 1, 0.5, 0.5)
+var BlueTColor: Color = Color(0.5, 1, 1, 0.5)
+
+# Constants
+const BOARD_SIZE = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,29 +26,67 @@ func _ready():
 	viewport.size_changed.connect(viewport_Changed)
 	setup(viewport.size)
 
-func viewport_Changed(size: Vector2 = Vector2()):
+func viewport_Changed():
 	setup(viewport.size)
 
 func setup(size: Vector2):
-	boardSize = min(size.x, size.y)
-	cellSize = boardSize / 10
+	boardSize_max = max(size.x, size.y) * 0.9
+	boardSize_min = min(size.x, size.y)
+	cellSize_max = boardSize_max / BOARD_SIZE
+	cellSize_min = boardSize_min / BOARD_SIZE
 	boardCenter = size / 2
-	cellsStartOffset = Vector2(cellSize * 5, cellSize * 5)
+	cellsStartOffset = Vector2(cellSize_min * BOARD_SIZE / 2, cellSize_max * BOARD_SIZE / 2) if size.x < size.y else Vector2(cellSize_max * BOARD_SIZE / 2, cellSize_min * BOARD_SIZE / 2)
+	scale = Vector2(cellSize_min / 100, cellSize_max / 100) if size.x < size.y else Vector2(cellSize_max / 100, cellSize_min / 100)
 	BoardStart = boardCenter - cellsStartOffset
-	scale = Vector2(1, 1) * cellSize / 100
 
-	for i in range(10):
-		for j in range(10):
-			var index = j * 10 + i
-			var instance
-			if pooledCells.size() < 100:
+	var index 
+	var instance
+
+	for i in range(BOARD_SIZE):
+		for j in range(BOARD_SIZE):
+			
+			if pooledCells.size() < BOARD_SIZE * BOARD_SIZE:
 				instance = cellSprite.instantiate()
-				if (i + j) % 2 != 0:
-					instance.get_node("Sprite").modulate = Color(0, 0, 0)
+				instance.get_node("Sprite").modulate = BlackColor if (i + j) % 2 != 0 else Color(1, 1, 1, 1)
 				pooledCells.append(instance)
 			else:
+				index = getIndex(j,i)
 				instance = pooledCells[index]
+
 			instance.scale = scale
-			instance.position = BoardStart + Vector2(cellSize * i, cellSize * j)
+			cellsOffset = Vector2(cellSize_min * i, cellSize_max * j) if size.x < size.y else Vector2(cellSize_max * i, cellSize_min * j)
+			instance.position = BoardStart + cellsOffset
+
 			if not instance.is_inside_tree():
 				add_child(instance)
+
+	cellsOffset = Vector2(cellSize_min, cellSize_max) if size.x < size.y else Vector2(cellSize_max, cellSize_min)
+
+func getIndex(i: int, j: int) -> int:
+	return i + (j * BOARD_SIZE)
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event.is_pressed() and event is InputEventMouseButton:
+		var pos = Vector2i(event.position - BoardStart)
+		if pos.x < 0 or pos.y < 0:
+			return
+		var index_i = pos.y / int(cellsOffset.y)
+		var index_j = pos.x / int(cellsOffset.x)
+		if index_i >= BOARD_SIZE or index_j >= BOARD_SIZE:
+			return
+		var index = getIndex(index_i, index_j)
+		var currentTColor = RedTColor if isRedTurn else BlueTColor
+		var NcurrentTColor = BlueTColor if isRedTurn else RedTColor
+		var cellSpriteNode = pooledCells[index].get_node("Sprite")
+
+		if cellSpriteNode.modulate == NcurrentTColor:
+			return
+		elif cellSpriteNode.modulate == currentTColor:
+			print("Multi Click at: ", index_i, " ", index_j)
+			# TO DO add click for multiple times
+		else:
+			print("Mouse Click at: ", index_i, " ", index_j)
+			cellSpriteNode.modulate = currentTColor
+
+		isRedTurn = !isRedTurn
